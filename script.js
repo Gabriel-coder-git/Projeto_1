1cde37257fb8b628bb03069a2c170967adf68558
Gabriel <gabrieljoschem@gmail.com>
2025-12-23 17:53:56 -0300

    Jscript criação

---
+++ script.js
@@ -0,0 +1,12 @@
+function RegistraUsuario(){
+    const nome = document.getElementById("usuario").value;
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
+    const csenha = document.getElementById("senhaconfirm").value;
+
+    console.log(nome);
+    console.log(email);
+    console.log(senha);
+    console.log(csenha);
+
+}
