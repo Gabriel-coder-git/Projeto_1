06a6c9584602ff072e212f52acafb855e1650614
Gabriel <gabrieljoschem@gmail.com>
2025-12-24 01:15:33 -0300

    Criação API de registro de dados

---
+++ Teste_1/src/main/java/com/example/teste_1/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.teste_1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/Hello")
+    public String hello() {
+        return "Backend Spring está vivo!";
+    }
+
+
+}
